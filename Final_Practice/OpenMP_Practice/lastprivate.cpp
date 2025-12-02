7282a121feb379c69650ed2b027087ee8f6fca88
root <root@ubuntu-22.04>
2025-12-02 06:08:54 +0000

    feat(OpenMP_Practice): add lastprivate.cpp

---
+++ Final_Practice/OpenMP_Practice/lastprivate.cpp
@@ -0,0 +1,16 @@
+#include <omp.h>
+#include <cstdio>
+#include <vector>
+
+int main() {
+	int i = 10;
+
+#pragma omp parallel for lastprivate(i)
+	for (int j = 0; j < 20; j++) {
+		printf("i: %d\n", i);
+		i = 20;
+	}
+
+	printf("current i: %d\n", i);
+}
+
